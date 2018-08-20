67a8f0f944fa128c2fe7a1e06ebd2593adc15084
starstarM <836408280@qq.com>
2018-08-20 15:05:48 +0800

    fix_isdigit

--- helper.py
+++ helper.py
@@ -5,6 +5,6 @@ def is_isbn_or_key(word):
     if len(word) == 13 and word.isdigit:
         isbn_or_key = 'isbn'
     short_word = word.replace('-', '')
-    if '-' in word and len(short_word) == 10 and short_word.digit:
+    if '-' in word and len(short_word) == 10 and short_word.isdigit:
         isbn_or_key = 'isbn'
     return isbn_or_key
